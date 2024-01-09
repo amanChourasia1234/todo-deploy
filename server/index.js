94738629be308fd8e5cead05c98af7dcac67aa66
Aman Chourasia <82589551+amanChourasia1234@users.noreply.github.com>
2024-01-09 22:13:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/todos', todosRoutes);
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 const mongodb =
