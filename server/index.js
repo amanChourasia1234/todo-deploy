808519b697f00fededcba9f686a591b58b378aa5
Aman Chourasia <82589551+amanChourasia1234@users.noreply.github.com>
2024-01-09 21:44:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import todosRoutes from './routes/todos.js';
+
 const app = express();
+import path from 'path';
+  
 dotenv.config();
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/todos', todosRoutes);
+
+app.use(express.static(path.join(__dirname, './client/build')));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 const mongodb =
   'mongodb+srv://aman:aman@cluster0.wgi0196.mongodb.net/?retryWrites=true&w=majority';
